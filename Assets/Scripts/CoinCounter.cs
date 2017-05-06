a9a5cb309e2e97853a784a8698e480b83546296c
shota elyanishvili <gunnershota@gmail.com>
2017-05-06 22:32:34 +0400

    trualebs bruanvs 1-2

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+
+	public int score = 0;
+	public Text scoreText;
+	public Text resultText;
+	public string coinTagString;
+	public string badCoinTagString;
+	public GameObject menu;
+	public PlayerMover mymover;
+	public Rigidbody body;
+	public string losestring;
+	public string winstring;
+	int targetscore;
+
+
+	void Start() {
+		targetscore = GameObject.FindGameObjectsWithTag(coinTagString).Length;
+		menu.SetActive (false);
+
+	}
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log("Collision " + col.gameObject.name);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == coinTagString) {
+			score = score + 1;
+		} else if (other.tag == badCoinTagString) {
+			menu.SetActive (true);
+			mymover.enabled = false;
+			body.isKinematic = true;
+			resultText.text = losestring;
+
+
+		}
+
+		Debug.Log ("Triggered " + score + " " + other.gameObject.name);
+
+		scoreText.text = "1st Score: " + score;
+
+
+		Destroy (other.gameObject);
+	}
+
+
+
+		void Update() {
+		
+		if (menu.activeSelf) {
+			mymover.enabled = false;
+			body.isKinematic = true;
+		} else if (GameObject.FindGameObjectsWithTag (coinTagString).Length == 0) {
+			menu.SetActive (true);
+			mymover.enabled = false;
+			body.isKinematic = true;
+			if (score > targetscore / 2) {
+
+				resultText.text = winstring;
+				
+			} else {
+				resultText.text = losestring;
+			}
+
+
+
+		} else if (GameObject.FindGameObjectsWithTag (badCoinTagString).Length == 10) {
+			menu.SetActive (true);
+			mymover.enabled = false;
+			body.isKinematic = true;
+		}
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/CoinCounter1.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter1 : MonoBehaviour
+{
+
+	public int score1 = 0;
+	public Text scoreText1;
+	public Text resultText1;
+	public string coinTagString1;
+	public string badCoinTagString1;
+	public GameObject menu;
+	public playermover2 mymover1;
+	public Rigidbody body1;
+	public string losestring1;
+	public string winstring1;
+	int targetscore1;
+	void Start() {
+		targetscore1 = GameObject.FindGameObjectsWithTag(coinTagString1).Length;
+		menu.SetActive (false);
+
+	}
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log("Collision " + col.gameObject.name);
+	}
+
+	void OnTriggerEnter(Collider other1)
+	{
+		if (other1.tag == coinTagString1) {
+			score1 = score1 + 1;
+		} else if (other1.tag == badCoinTagString1) {
+			menu.SetActive (true);
+			mymover1.enabled = false;
+			body1.isKinematic = true;
+			resultText1.text = losestring1;
+
+
+		}
+
+		Debug.Log ("Triggered " + score1 + " " + other1.gameObject.name);
+
+		scoreText1.text = "2nd Score: " + score1;
+
+
+		Destroy (other1.gameObject);
+	}
+
+
+
+	void Update() {
+
+
+		if (menu.activeSelf) {
+			mymover1.enabled = false;
+			body1.isKinematic = true;
+		}
+
+		 else if (GameObject.FindGameObjectsWithTag (coinTagString1).Length == 0) {
+			menu.SetActive (true);
+			mymover1.enabled = false;
+			body1.isKinematic = true;
+			if (score1 > targetscore1 / 2) {
+
+				resultText1.text = winstring1;
+
+			} else {
+				resultText1.text = losestring1;
+			}
+
+
+
+		}
+		else if (GameObject.FindGameObjectsWithTag (badCoinTagString1).Length ==  10) {
+			menu.SetActive (true);
+			mymover1.enabled = false;
+			body1.isKinematic = true;
+
+	}
+}
+}
+
+
+
+
+
+
+
+
+
+
